c19f3d6ead43492d2bf8271d45cb68f9b9218959
adnanabh <adnan.muslija@atlantbh.com>
2016-03-13 17:00:09 +0000

    potential fix for superuser

--- networkinstitute/models.py
+++ networkinstitute/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.http import urlquote
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils import timezone
 from django.core.mail import send_mail
 from django.utils.translation import ugettext_lazy as _
