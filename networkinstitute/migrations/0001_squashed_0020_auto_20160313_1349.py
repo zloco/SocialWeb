a47b4bb6fcb17d3fbb37d981b74931a641315fdd
zloco <aploco1@etf.unsa.ba>
2016-03-15 13:42:53 +0100

    altered squashmigrations

--- networkinstitute/migrations/0001_squashed_0020_auto_20160313_1349.py
+++ networkinstitute/migrations/0001_squashed_0020_auto_20160313_1349.py
@@ -112,6 +112,11 @@ class Migration(migrations.Migration):
             name='is_staff',
             field=models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status'),
         ),
+		migrations.AddField(
+            model_name='customuser',
+            name='is_superuser',
+            field=models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status'),
+        ),
         migrations.AlterField(
             model_name='project',
             name='members',
@@ -126,11 +131,6 @@ class Migration(migrations.Migration):
             model_name='customuser',
             name='groups',
             field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to=b'auth.Group', verbose_name='groups'),
-        ),
-        migrations.AddField(
-            model_name='customuser',
-            name='is_superuser',
-            field=models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status'),
         ),
         migrations.AddField(
             model_name='customuser',
